bf06452d3a8248ebe9379767c57be758a68a731b
Heidi Boulter <boulter.heidi@gmail.com>
2019-10-10 19:22:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   counter = 0
-  array = numbers
-  numbers = [1,2,3]
-  while numbers[counter] do
-    puts numbers[counter]
-    numbers ** 2
+  numbers = array
+  array = [1,2,3]
+  while array[counter] do
+    puts array[counter]
+    array ** 2
   end
 end
